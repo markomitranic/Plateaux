c3740cb9ddc817cabca21b9d198f153f857ea7a2
Marko Mitranic <marko.mitranic@catenamedia.com>
2017-06-09 22:34:54 +0200

    Rename server file and change port to 3000

--- app/plateaux.js
+++ app/plateaux.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
-app.listen(80, function () {
-    console.log('Example app listening on port 80!')
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
 })
