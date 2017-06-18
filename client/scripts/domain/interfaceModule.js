d1b3fe13199646864e1c69311fef9b8bf19809d0
Marko Mitranic <marko.mitranic@catenamedia.com>
2017-06-18 11:57:47 +0200

    Add sprites and styles for menu buttons

--- client/scripts/domain/interfaceModule.js
+++ client/scripts/domain/interfaceModule.js
@@ -3,8 +3,11 @@ const muteButton = document.querySelectorAll('#controls .muteButton')[0];
 muteButton.addEventListener('click', (e) => {
     if (audioMuted) {
         audioMuted = false;
+        muteButton.classList.remove("muted");
     } else {
         audioMuted = true;
+        muteButton.classList.add("muted");
     }
     sound.mute(audioMuted);
 });
+
